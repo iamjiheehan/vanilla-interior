e320a5ccdb43e2e68371e5cb6ececf2b8d7b762e
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-04 20:50:42 +0900

    import에서 require로 대체

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import Home from "./pages/Home.js";
-import Design from "./pages/Design.js";
-import Board from "./pages/Board.js";
-import Submit from "./pages/Submit.js";
+const Home = require("./pages/Home.js").default;
+const Design = require("./pages/Design.js").default;
+const Board = require("./pages/Board.js").default;
+const Submit = require("./pages/Submit.js").default;
 
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
