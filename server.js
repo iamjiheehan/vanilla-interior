766ff9bee23116c26c3400f3f4f1a5eeca1ca3f0
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-05 23:25:49 +0900

    Updated netlify.toml

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const mime = require('mime');
+const path = require('path');
 
 const app = express();
 const __dirname = path.resolve();
 
+// Serve static files from the public folder
 app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Serve JavaScript file with correct MIME type
+app.get('/public/index.js', (req, res) => {
+    const filePath = path.resolve(__dirname, 'public', 'index.js');
+    const mimeType = mime.getType(filePath);
+
+    res.set('Content-Type', mimeType);
+    res.sendFile(filePath);
+});
 
 // Serve the index.html file with the script for the Design.js file
 app.get('/design', (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"), {
-        scripts: [{src: '/node_modules/drag-drop-touch/src/DragDropTouch.js', type: 'module'}]
+        scripts: [{src: '/node_modules/drag-drop-touch/DragDropTouch.js', type: 'module'}]
     });
 });
 
+// Serve index.html for all other routes
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "public","index.html"));
 });
 
-app.listen(process.env.PORT || 3000, ()=>{
+// Start server
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port 3000");
 });
