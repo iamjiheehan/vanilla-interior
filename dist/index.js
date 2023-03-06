b6d42c02949b8b238716eefc2d96b9791d066d4e
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-07 00:08:35 +0900

    Updated redirect

--- dist/index.js
+++ dist/index.js
@@ -9,9 +9,6 @@ const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(
 const getParams = match => {
   const values = match.result.slice(1);
   const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
-
-  // console.log(Array.from(match.route.path.matchAll(/:(\w+)/g)));
-
   return Object.fromEntries(keys.map((key, i) => {
     return [key, values[i]];
   }));
@@ -21,7 +18,6 @@ const navigateTo = url => {
   router();
 };
 const router = async () => {
-  // console.log(pathToRegex("/design/:id"));
   const routes = [{
     path: "/",
     pages: _Home.default
--- public/index.js
+++ public/index.js
@@ -9,8 +9,6 @@ const getParams = match => {
     const values = match.result.slice(1);
     const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
 
-    // console.log(Array.from(match.route.path.matchAll(/:(\w+)/g)));
-
     return Object.fromEntries(keys.map((key, i) => {
         return [key, values[i]];
     }));
@@ -22,7 +20,6 @@ const navigateTo = url => {
 }
 
 const router = async () => {
-    // console.log(pathToRegex("/design/:id"));
     const routes = [
         { path : "/", pages : Home},
         { path : "/design", pages : Design},
@@ -48,13 +45,6 @@ const router = async () => {
     const pages = new match.route.pages(getParams(match));
     
 
-    if (window.navigator.userAgentData && window.navigator.userAgentData.brands.length > 0) {
-        const browserName = window.navigator.userAgentData.brands[0].brand;
-        const version = parseInt(window.navigator.userAgentData.brands[0].version, 10);
-        if (browserName === "Chrome" && version >= 101) {
-            console.log("Chrome 101 or later detected. Use navigator.userAgentData instead of navigator.userAgent, navigator.appVersion, and navigator.platform.");
-        }
-    }
 
     document.querySelector("#app").innerHTML = await pages.getHTML();
     await pages.executeScript();
